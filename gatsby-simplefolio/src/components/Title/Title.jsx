56e9bb4ba7de2d844da2d6dcfacac794307a1d21
DustinTheGreat <dustinroberts96@gmail.com>
2021-10-07 17:19:02 -0400

    Update Title.jsx

--- gatsby-simplefolio/src/components/Title/Title.jsx
+++ gatsby-simplefolio/src/components/Title/Title.jsx
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 
 const Title = ({ title }) => (
   <Fade bottom duration={1000} delay={300} distance="0px">
-    <h1>CHANDLER (the bulldog) Burket</h1>
     <h2 className="section-title">{title}</h2>
   </Fade>
 );
